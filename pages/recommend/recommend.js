63c1e73bdb85772bf883cd96b5820d27c02a9baf
youknowwhom <2154312@tongji.edu.cn>
2024-04-18 09:55:40 +0800

    add a simple slot machine demo

--- pages/recommend/recommend.js
+++ pages/recommend/recommend.js
@@ -1,11 +1,33 @@
-// pages/recommend/recommend.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    scrollText: ['黄骨鱼粉', '酸菜鱼粉', '鱼肉粉', '霸气鱼头粉', '辣椒炒肉干拌粉', '永州麻鸭干拌粉', '一碗冰粉', '一碗冰豆花'], // 滚动的文字
+    animationData: null // 绑定的动画效果
+  },
+  // 开始滚动
+  startScroll () {
+    // 创建一个动画实例
+    let animation = wx.createAnimation({
+      duration: 5000,
+      timingFunction: 'ease'
+    })
+    // 获取元素总高度
+    let height =  (this.data.scrollText.length - 1) * 300
+    var random = Math.floor(Math.random()*this.data.scrollText.length) * 300;
+    // 向上移动
+    animation.translateY(-random + 'rpx').step()
+    // 将动画效果赋值
+    this.setData({
+      animationData: animation.export()
+    })
+  },
+  // 重置
+  reset () {
+    let animation = wx.createAnimation({
+      duration: 0
+    })
+    this.setData({
+      animationData: animation.translateY(0).step().export()
+    })
   },
 
   /**
