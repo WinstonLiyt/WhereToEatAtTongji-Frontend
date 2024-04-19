cd077c3491c612d46c5e2e896686dbd1eea539db
youknowwhom <2154312@tongji.edu.cn>
2024-04-20 02:41:03 +0800

    add student user profile page

---
+++ pages/personal-student/personal-student.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    username: "梅毛冰",
+    avatar: "/statics/imgs/community/avatar.jpg",
+    signature: "For the hope of it all.",
+    post: "25",
+    follow: "1",
+    fan: 520,
+  },
+});
