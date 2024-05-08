dfe673d59a43159e91ec1ce34ab2a4b26034bb78
WinstonLiyt <1957922024@qq.com>
2024-05-08 20:45:53 +0800

    Merchant -- Choose Labels Debug

--- pages/business_edit/business_edit.js
+++ pages/business_edit/business_edit.js
@@ -8,17 +8,42 @@ Page({
     post: "25",
     follow: "1",
     fan: 520,
-    tagOptions: ["汉堡", "快餐", "中餐", "火锅", "披萨", "饮品", "水果"],
-    tagIndex: [], // 用户选择的标签索引
-    selectedTags: [] // 用户选择的标签
+    tagOptions: [
+      {value: "1", label: "汉堡"},  
+      {value: "2", label: "快餐"},  
+      {value: "3", label: "中餐"},
+      {value: "4", label: "火锅"},
+      {value: "5", label: "披萨"},
+      {value: "6", label: "饮品"},
+      {value: "7", label: "水果"}
+    ],
+    slicedTagOptions: [], // 用于存储从第三个元素开始的切片数组  
+    selectedTags: [], // 用户选择的标签
+    showDropdown: false // 控制下拉展开状态
   },
 
+  onLoad: function() {  
+    // 在页面加载时，从第三个元素开始切片数组  
+    this.setData({  
+      slicedTagOptions: this.data.tagOptions.slice(2) // 索引从0开始，所以2表示第三个元素  
+    });  
+  },  
+  
   handleTagChange: function (e) {
+    const tagIndex = e.detail.value;
+    const selectedTags = tagIndex.map(index => this.data.tagOptions[index]);
     this.setData({
-      tagIndex: e.detail.value,
-      selectedTags: e.detail.value.map(index => this.data.tagOptions[index])
+      // tagIndex: tagIndex,
+      // selectedTags: selectedTags
+      selectedTags: e.detail.value  
     });
   },
+
+  toggleDropdown: function () {
+    this.setData({
+      showDropdown: !this.data.showDropdown
+    });
+  },  
 
   navigateToHome: function() {
     console.log('Attempting to navigate to personal page');
@@ -29,6 +54,4 @@ Page({
       }
     });
   },
-  
-
 });
