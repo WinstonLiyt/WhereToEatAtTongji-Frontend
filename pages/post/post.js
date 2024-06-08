22cfa0b26757fa87310d5825af5d8570d3f75e26
Nozom1466 <98678848+Nozom1466@users.noreply.github.com>
2024-06-08 18:25:13 +0800

    修改换行符不合法问题

--- pages/post/post.js
+++ pages/post/post.js
@@ -657,7 +657,7 @@ Component({
             });
         },
         checkVdility(str) {
-            if (/[^a-zA-Z0-9\u4e00-\u9fa5,.!?，。！？]/.test(str)) {
+            if (/[^a-zA-Z0-9\u4e00-\u9fa5,.!?，。！？\n]/.test(str)) {
                 return false;
             }
             return true;
